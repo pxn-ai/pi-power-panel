3343a3d945ddc0a951edab800734b76e0d5185b1
pasan_r_k_95 <kaluarachchikapr.24@uom.lk>
2026-05-09 09:57:17 +0530

    security fix

--- server/pi-agent.ts
+++ server/pi-agent.ts
@@ -11,6 +11,7 @@ Bun.serve({
     const headers = {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "*",
       "Content-Type": "application/json"
     };
 
